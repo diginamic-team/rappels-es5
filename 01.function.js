8ae3c28b2c2340912055a377c08378dec7116bac
unknown <alex.ayato@gmail.com>
2019-03-21 10:52:21 +0100

    premier ordre commencé

---
+++ 01.function.js
@@ -0,0 +1,32 @@
+console.log("01 - Fonctions");
+
+nombre1 = 10;
+nombre2 = 20;
+
+function additionner (n1, n2){
+    return n1 + n2;
+}
+var somme = additionner;
+
+resultat2 = somme(nombre1, nombre2);
+
+console.log(resultat2);
+
+function multiplication(n1, n2){
+    return n1 * n2;
+}
+
+resultat3 = multiplication(nombre1, nombre2);
+
+console.log(resultat3);
+
+nomOperation = new String;
+operation = new Function;
+nb1 = 10;
+nb2 = 20;
+
+function afficherOperation(nomOpe, Fonction, nombre01, nombre02){
+    return console.log(nomOpe + '(' + nombre01 + ',' + nombre02 + ')' + " = " + Fonction(nombre01, nombre02) );
+}
+
+afficherOperation("Addition", additionner, nb1, nb2);
